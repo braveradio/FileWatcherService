7009f61cf046154ea906f28ad728a1188519fb0a
Ильнур Гиззатуллин <juvebars@mail.ru>
2020-09-21 13:59:03 +0300

    Save feature added.

---
+++ FileWatcherService/SaveToFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherService
+{
+    public class SaveToFile : ISaveInfo
+    {
+        public void Save()
+        {
+            Console.WriteLine("Save to txt file...");
+        }
+    }
+}
